2f6c56e954741275984c1f119afc115263e99886
James Kong <james.kong@nixplay.com>
2017-04-20 12:23:59 +0800

    init

---
+++ sync_plugin.sh
@@ -0,0 +1,4 @@
+# cp /Users/jameskong/Development/ionic/NativeUIExample/platforms/ios/NativeUIExample/Plugins/com.wunwun.plugins.mw-photo-browser-cordova/*  /Users/jameskong/Development/nix/b2c/cordova-plugin-mw-photo-browser/src/ios/
+# cp -r /Users/jameskong/Development/ionic/NativeUIExample/platforms/ios/Pods/MWPhotoBrowser/Pod/Classes /Users/jameskong/Development/nix/b2c/MWPhotoBrowser/Pod/
+cp -r /Users/jameskong/Development/ionic/NativeUIExample/platforms/ios/NativeUIExample/Plugins/cordova-plugin-caption-input/* /Users/jameskong/Development/nix/b2c/cordova-plugin-caption-input/src/ios/
+cp /Users/jameskong/Development/ionic/NativeUIExample/plugins/cordova-plugin-caption-input/www/caption-input.js /Users/jameskong/Development/nix/b2c/cordova-plugin-caption-input/www/caption-input.js
---
+++ www/js/photoProvider.js
@@ -0,0 +1,14 @@
+/*
+myApp.controller('DemoController', ['clientId', function DemoController(clientId) {
+  this.clientId = clientId;
+}]);
+*/
+
+angular.module(
+	'starter.photoProvider',
+	[])
+.controller('photoProvider', function ($scope){
+	console.log("photoProvider " +this);
+	
+
+})
